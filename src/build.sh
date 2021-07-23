9f25f2152803398b6cbfe2b87146f48a92f0c60c
Xliff <clifton.wood@gmail.com>
2021-07-23 18:27:29 -0400

    - Updated src/build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-gcc Xlib-helper.c -shared -fPIC `pkg-config --libs x11` -o Xlib-helper.so
+#gcc Xlib-helper.c --shared `xpkg-config --cflags xt` -fPIC `pkg-config --libs xt` -o Xlib-helper.so
+gcc Xlib-helper.c -I/usr/include/X11 `pkg-config --cflags xt` `pkg-config --libs xt` -o x11-info
